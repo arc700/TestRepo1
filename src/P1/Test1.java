bc584a5767f7c58b762ca529094a13e5bef00eea
ArchanaJadhav <archananpawar11@gmail.com>
2022-01-23 09:08:01 +0530

    main

---
+++ src/P1/Test1.java
@@ -0,0 +1,9 @@
+package P1;
+
+public class Test1 {
+	static public void main(String[] args) {
+
+		System.out.println("In Test 1");
+
+	}
+}
---
+++ src/P2/Test2.java
@@ -0,0 +1,9 @@
+package P2;
+
+public class Test2 {
+	static public void main(String[] args) {
+
+		System.out.println("In Test 2");
+
+	}
+}
